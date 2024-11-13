2eefac75e46ac19b55d29a2253e32991c8e918fa
Dewey Dunnington <dewey@voltrondata.com>
2024-11-13 16:56:33 -0600

    document it

--- src/read_arrow_stream.cpp
+++ src/read_arrow_stream.cpp
@@ -11,6 +11,20 @@
 #include "nanoarrow/nanoarrow.hpp"
 #include "nanoarrow/nanoarrow_ipc.hpp"
 
+// read_arrow_stream() implementation
+//
+// This currently uses the "easy" IPC Reader route, which wraps
+// an ArrowIpcInputStream (wrapper around a FileHandle) with an
+// ArrowArrayStream implementation. This works but involves copying
+// quite a lot of DuckDB's internals and doesn't use DuckDB's allocator,
+// Really this should use the ArrowIpcEncoder() and implement the various
+// pieces of the scan specific to Arrow IPC.
+//
+// DuckDB could improve this process by making it easier
+// to build a file scanner around an ArrowArrayStream; nanoarrow could make
+// this easier by allowing the ArrowIpcArrayStreamReader to plug in an
+// ArrowBufferAllocator().
+
 #define _DUCKDB_NANOARROW_THROW_NOT_OK_IMPL(NAME, ExceptionCls, ERROR_PTR, EXPR,      \
                                             EXPR_STR)                                 \
   do {                                                                                \
@@ -32,9 +46,15 @@ namespace ext_nanoarrow {
 
 namespace {
 
+// Initializes our ArrowIpcInputStream wrapper from DuckDB's file
+// abstraction. This lets us use their filesystems and any plugins that
+// add them (like httpfs).
 inline void InitDuckDBInputStream(unique_ptr<FileHandle> handle,
                                   ArrowIpcInputStream* out);
 
+// This Factory is a type invented by DuckDB. Notably, the Produce()
+// function pointer is passed to the constructor of the ArrowScanFunctionData
+// constructor (which we wrap).
 class ArrowIpcArrowArrayStreamFactory {
  public:
   explicit ArrowIpcArrowArrayStreamFactory(ClientContext& context,
@@ -65,10 +85,12 @@ class ArrowIpcArrowArrayStreamFactory {
       throw InternalException("ArrowArrayStream was released by another thread/library");
     }
 
-    THROW_NOT_OK(InternalException, &error,
+    THROW_NOT_OK(IOException, &error,
                  ArrowArrayStreamGetSchema(stream.get(), &schema.arrow_schema, &error));
   }
 
+  // Opens the file, wraps it in the ArrowIpcInputStream, and wraps it in
+  // the ArrowArrayStream reader.
   void InitStream() {
     if (stream->release) {
       throw InternalException("ArrowArrayStream is already initialized");
@@ -95,16 +117,24 @@ class ArrowIpcArrowArrayStreamFactory {
 };
 
 struct ReadArrowStream {
+  // Register the function. Unlike arrow_scan(), which takes integer pointers
+  // as arguments, we keep the factory alive by making it a member of the bind
+  // data (instead of as a Python object whose ownership is kept alive via the
+  // DependencyItem mechanism).
   static void Register(DatabaseInstance& db) {
     TableFunction fn("read_arrow_stream", {LogicalType::VARCHAR}, Scan, Bind,
                      ArrowTableFunction::ArrowScanInitGlobal,
                      ArrowTableFunction::ArrowScanInitLocal);
-    fn.projection_pushdown = false;
+    fn.get_batch_index = GetBatchIndex;
+    fn.cardinality = Cardinality;
+    fn.projection_pushdown = true;
     fn.filter_pushdown = false;
     fn.filter_prune = false;
     ExtensionUtil::RegisterFunction(db, fn);
   }
 
+  // Our FunctionData is the same as the ArrowScanFunctionData except we extend it
+  // it to keep the ArrowIpcArrowArrayStreamFactory alive.
   struct Data : public ArrowScanFunctionData {
     Data(std::unique_ptr<ArrowIpcArrowArrayStreamFactory> factory)
         : ArrowScanFunctionData(ArrowIpcArrowArrayStreamFactory::Produce,
@@ -113,6 +143,8 @@ struct ReadArrowStream {
     std::unique_ptr<ArrowIpcArrowArrayStreamFactory> factory;
   };
 
+  // Our Bind() function is differenct from the arrow_scan because our input
+  // is a filename (and their input is three pointer addresses).
   static unique_ptr<FunctionData> Bind(ClientContext& context,
                                        TableFunctionBindInput& input,
                                        vector<LogicalType>& return_types,
@@ -136,11 +168,63 @@ struct ReadArrowStream {
     return unique_ptr<FunctionData>(res.release());
   }
 
-  static void Scan(ClientContext& context, TableFunctionInput& input, DataChunk& output) {
-    ArrowTableFunction::ArrowScanFunction(context, input, output);
+  // This is almost the same as ArrowTableFunction::Scan() except we need to pass
+  // arrow_scan_is_projected = false to ArrowToDuckDB(). It's a bit unfortunate
+  // we have to copy this much (although the spatial extension also copies this
+  // as it does something vaguely similar).
+  static void Scan(ClientContext& context, TableFunctionInput& data_p,
+                   DataChunk& output) {
+    if (!data_p.local_state) {
+      return;
+    }
+    auto& data = data_p.bind_data->CastNoConst<ArrowScanFunctionData>();  // FIXME
+    auto& state = data_p.local_state->Cast<ArrowScanLocalState>();
+    auto& global_state = data_p.global_state->Cast<ArrowScanGlobalState>();
+
+    //! Out of tuples in this chunk
+    if (state.chunk_offset >= (idx_t)state.chunk->arrow_array.length) {
+      if (!ArrowTableFunction::ArrowScanParallelStateNext(context, data_p.bind_data.get(),
+                                                          state, global_state)) {
+        return;
+      }
+    }
+    auto output_size = MinValue<idx_t>(
+        STANDARD_VECTOR_SIZE,
+        NumericCast<idx_t>(state.chunk->arrow_array.length) - state.chunk_offset);
+    data.lines_read += output_size;
+    if (global_state.CanRemoveFilterColumns()) {
+      state.all_columns.Reset();
+      state.all_columns.SetCardinality(output_size);
+      ArrowTableFunction::ArrowToDuckDB(state, data.arrow_table.GetColumns(),
+                                        state.all_columns, data.lines_read - output_size,
+                                        false);
+      output.ReferenceColumns(state.all_columns, global_state.projection_ids);
+    } else {
+      output.SetCardinality(output_size);
+      ArrowTableFunction::ArrowToDuckDB(state, data.arrow_table.GetColumns(), output,
+                                        data.lines_read - output_size, false);
+    }
+
+    output.Verify();
+    state.chunk_offset += output.size();
+  }
+
+  // Identical to the ArrowTableFunction, but that version is marked protected
+  static idx_t GetBatchIndex(ClientContext& context, const FunctionData* bind_data_p,
+                             LocalTableFunctionState* local_state,
+                             GlobalTableFunctionState* global_state) {
+    auto& state = local_state->Cast<ArrowScanLocalState>();
+    return state.batch_index;
+  }
+
+  // Identical to the ArrowTableFunction, but that version is marked protected
+  static unique_ptr<NodeStatistics> Cardinality(ClientContext& context,
+                                                const FunctionData* data) {
+    return make_uniq<NodeStatistics>();
   }
 };
 
+// Implementation of the ArrowIpcInputStream wrapper around DuckDB's input stream
 struct DuckDBArrowInputStream {
   unique_ptr<FileHandle> handle;
 
