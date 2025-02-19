8641e99aa86f64dc50247e9944f949d07b5de6f7
pdet <pedroholanda@gmail.com>
2025-02-19 08:49:47 -0300

    Add Python CI, and a simple single-batch test

--- test/python/test_arrow_ipc_scan.py
+++ test/python/test_arrow_ipc_scan.py
@@ -1,38 +1,35 @@
+import pytest
 import pyarrow as pa
 import duckdb
 import pyarrow.parquet as pq
 
 
-con = duckdb.connect(config={"allow_unsigned_extensions":"true"})
+class TestArrowIPCBufferRead(object):
+   def test_single_batch(self, connection):
+      data = [
+          pa.array([1, 2, 3, 4]),
+          pa.array(['foo', 'bar', 'baz', None]),
+          pa.array([True, None, False, True])
+      ]
 
-# con.execute("load 'arrow'")
+      batch = pa.record_batch(data, names=['f0', 'f1', 'f2'])
 
-con.execute("load '/Users/holanda/Documents/Projects/duckdb-arrow/build/release/extension/nanoarrow/nanoarrow.duckdb_extension'")
+      buffers = []
 
-data = [
-    pa.array([1, 2, 3, 4]),
-    pa.array(['foo', 'bar', 'baz', None]),
-    pa.array([True, None, False, True])
-]
+      sink = pa.BufferOutputStream()
+      with pa.ipc.new_stream(sink, batch.schema) as writer:
+         for i in range(5):
+            writer.write_batch(batch)
 
-batch = pa.record_batch(data, names=['f0', 'f1', 'f2'])
+      buffers.append(sink.getvalue())
 
-buffers = []
+      structs = ''
+      for buffer in buffers:
+          structs = structs + f"{{'ptr': {buffer.address}::UBIGINT, 'size': {buffer.size}::UBIGINT}},"
 
-sink = pa.BufferOutputStream()
-with pa.ipc.new_stream(sink, batch.schema) as writer:
-   for i in range(5):
-      writer.write_batch(batch)
+      structs = structs[:-1]
+      print(structs)
 
-buffers.append(sink.getvalue())
+      arrow_scan_function = f"FROM scan_arrow_ipc([{structs}])"
 
-structs = ''
-for buffer in buffers:
-    structs = structs + f"{{'ptr': {buffer.address}::UBIGINT, 'size': {buffer.size}::UBIGINT}},"
-
-structs = structs[:-1]
-print(structs)
-
-arrow_scan_function = f"FROM scan_arrow_ipc([{structs}])"
-
-print(con.execute(arrow_scan_function).fetchall())
+      connection.execute(arrow_scan_function).fetchall() == [(1, 'foo', True), (2, 'bar', None), (3, 'baz', False), (4, None, True), (1, 'foo', True), (2, 'bar', None), (3, 'baz', False), (4, None, True), (1, 'foo', True), (2, 'bar', None), (3, 'baz', False), (4, None, True), (1, 'foo', True), (2, 'bar', None), (3, 'baz', False), (4, None, True), (1, 'foo', True), (2, 'bar', None), (3, 'baz', False), (4, None, True)]
