6f216805aedfb7133e9872552fd7efe4e7043c7f
pdet <pedroholanda@gmail.com>
2025-03-14 13:41:12 +0100

    All big-little endian tests for file IPC working

--- test/python/test_integration.py
+++ test/python/test_integration.py
@@ -10,21 +10,17 @@ import tempfile
 # duckdb.duckdb.NotImplementedException: Not implemented Error: Unsupported Internal Arrow Type for Decimal d:37,5,256
 # "generated_decimal256.stream",
 
-# duckdb.duckdb.InternalException: INTERNAL Error: Field 'ints' refers to a duplicate column name in IPC file schema
-# "generated_duplicate_fieldnames.stream",
-
 # duckdb.duckdb.ConversionException: Conversion Error: Could not convert Interval to Microsecond
 # "generated_interval.stream"
 
-# Attempted to dereference shared_ptr that is NULL
-# "generated_null_trivial.stream"
-
 # Not implemented Error: Unsupported Internal Arrow Type: "d" Union
 # "generated_union.stream"
+# "0.17.1/generated_union.stream"
 
-# "0.17.1/generated_union.stream","0.14.1/generated_datetime.stream","0.14.1/generated_decimal.stream","0.14.1/generated_interval.stream","0.14.1/generated_map.stream","0.14.1/generated_nested.stream","0.14.1/generated_primitive.stream","0.14.1/generated_primitive_no_batches.stream","0.14.1/generated_primitive_zerolength.stream"
+little_big_integration_files = ["generated_null_trivial.stream", "generated_primitive_large_offsets.stream","generated_custom_metadata.stream","generated_datetime.stream","generated_decimal.stream","generated_map_non_canonical.stream","generated_map.stream","generated_nested_large_offsets.stream","generated_nested.stream","generated_null.stream","generated_primitive_no_batches.stream","generated_primitive_zerolength.stream","generated_primitive.stream","generated_recursive_nested.stream"]
 
-integration_files = ["generated_primitive_large_offsets.stream","generated_custom_metadata.stream","generated_datetime.stream","generated_decimal.stream","generated_map_non_canonical.stream","generated_map.stream","generated_nested_large_offsets.stream","generated_nested.stream","generated_null.stream","generated_primitive_no_batches.stream","generated_primitive_zerolength.stream","generated_primitive.stream","generated_recursive_nested.stream"]
+# IO Error: Expected continuation token (0xFFFFFFFF) but got XXX
+integration_files_0_14_1 = ["generated_datetime.stream","generated_decimal.stream","generated_interval.stream","generated_map.stream","generated_nested.stream","generated_primitive.stream","generated_primitive_no_batches.stream","generated_primitive_zerolength.stream"]
 
 
 script_path = os.path.dirname(os.path.abspath(__file__))
@@ -91,25 +87,51 @@ def compare_ipc_buffer_writer(con, file):
 
 
 class TestArrowIntegrationTests(object):
-    def test_bigendian_integration_file_reader(self, connection):
-        big_endian_folder = os.path.join(test_folder,'1.0.0-bigendian')
-        for file in integration_files:
-            compare_ipc_file_reader(connection,os.path.join(big_endian_folder,file))
 
-    def test_bigendian_integration_file_writer(self, connection):
+    def test_read_ipc_file(self, connection):
         big_endian_folder = os.path.join(test_folder,'1.0.0-bigendian')
-        for file in integration_files:
-            compare_ipc_file_writer(connection,os.path.join(big_endian_folder,file))
-
-    def test_littleendian_integration_file_reader(self, connection):
         little_endian_folder = os.path.join(test_folder,'1.0.0-littleendian')
-        for file in integration_files:
+        folder_0_14_1 = os.path.join(test_folder,'0.14.1')
+        for file in little_big_integration_files:
+            compare_ipc_file_reader(connection,os.path.join(big_endian_folder,file))
             compare_ipc_file_reader(connection,os.path.join(little_endian_folder,file))
 
-    def test_littleendian_integration_file_writer(self, connection):
+        # for file in integration_files_0_14_1:
+        #      compare_ipc_file_reader(connection,os.path.join(folder_0_14_1,file))
+
+    def test_write_ipc_file(self, connection):
+        big_endian_folder = os.path.join(test_folder,'1.0.0-bigendian')
         little_endian_folder = os.path.join(test_folder,'1.0.0-littleendian')
-        for file in integration_files:
+        for file in little_big_integration_files:
+            compare_ipc_file_writer(connection,os.path.join(big_endian_folder,file))
             compare_ipc_file_writer(connection,os.path.join(little_endian_folder,file))
+
+    # def test_read_ipc_buffer(self, connection):
+    #     big_endian_folder = os.path.join(test_folder,'1.0.0-bigendian')
+    #     little_endian_folder = os.path.join(test_folder,'1.0.0-littleendian')
+    #     for file in little_big_integration_files:
+    #         compare_ipc_buffer_reader(connection,os.path.join(big_endian_folder,file))
+    #         compare_ipc_buffer_reader(connection,os.path.join(little_endian_folder,file))
+
+    # def test_bigendian_integration_file_reader(self, connection):
+    #     big_endian_folder = os.path.join(test_folder,'1.0.0-bigendian')
+    #     for file in integration_files:
+    #         compare_ipc_file_reader(connection,os.path.join(big_endian_folder,file))
+
+    # def test_bigendian_integration_file_writer(self, connection):
+    #     big_endian_folder = os.path.join(test_folder,'1.0.0-bigendian')
+    #     for file in integration_files:
+    #         compare_ipc_file_writer(connection,os.path.join(big_endian_folder,file))
+
+    # def test_littleendian_integration_file_reader(self, connection):
+    #     little_endian_folder = os.path.join(test_folder,'1.0.0-littleendian')
+    #     for file in integration_files:
+    #         compare_ipc_file_reader(connection,os.path.join(little_endian_folder,file))
+
+    # def test_littleendian_integration_file_writer(self, connection):
+    #     little_endian_folder = os.path.join(test_folder,'1.0.0-littleendian')
+    #     for file in integration_files:
+    #         compare_ipc_file_writer(connection,os.path.join(little_endian_folder,file))
 
     # def test_bigendian_integration_buffer_reader(self, connection):
     #     big_endian_folder = os.path.join(test_folder,'1.0.0-bigendian')
