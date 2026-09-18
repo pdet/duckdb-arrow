feb834c4513bd7a4d161d6bce1060e8a88668225
Pedro Holanda <pedroholanda@gmail.com>
2026-09-18 21:01:14 +0200

    Build tpch and tpcds as test dependencies of the TPC tests instead of loading them in the extension config.

--- extension_config.cmake
+++ extension_config.cmake
@@ -8,6 +8,5 @@ duckdb_extension_load(nanoarrow
                       LINKED_LIBS
                       "../../_deps/nanoarrow-build/lib*.a")
 
-# The TPC tests generate their data and check their answers with these
-duckdb_extension_load(tpch)
-duckdb_extension_load(tpcds)
+# Any extra extensions that should be built
+# e.g.: duckdb_extension_load(json)
